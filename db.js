caff094fee3a8c77ffe57a9695110f3c307f55a5
rui030303 <rllu2@student.unimelb.edu.au>
2024-03-18 21:20:23 +0800

    update

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection(
     {
         host:"localhost",
         user:"root",
-        password:"",
+        password:"MYsql1998!",
         database:"blog_fullstack"
     }
 )
