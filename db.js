0bb601a335fbdc68ecbc3329c993fd7197d5c126
rui030303 <rllu2@student.unimelb.edu.au>
2024-03-19 15:30:28 +0800

    update db

--- db.js
+++ db.js
@@ -1,12 +1,19 @@
 import mysql from "mysql2"
 
+// const db = mysql.createConnection(
+//     {
+//         host:"localhost",
+//         user:"root",
+//         password:"MYsql1998!",
+//         database:"blog_fullstack"
+//     }
+// )
 const db = mysql.createConnection(
     {
-        host:"localhost",
-        user:"root",
-        password:"MYsql1998!",
+        host:"database-1.c1cmokc649lv.ap-southeast-2.rds.amazonaws.com",
+        user:"admin",
+        password:"951753123",
         database:"blog_fullstack"
     }
 )
-
 export default db
